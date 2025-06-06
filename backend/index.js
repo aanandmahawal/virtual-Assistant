d1e25acf6c3c2997c067bc4aae04970168b10cd6
aanandmahawal <151204835+aanandmahawal@users.noreply.github.com>
2025-06-07 01:02:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://virtual-assistant-0dz3.onrender.com",
     credentials:true
 }))
 const port=process.env.PORT || 5050
